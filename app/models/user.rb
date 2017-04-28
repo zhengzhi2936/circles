4ef4386ca9b5049e57d682fcb1b048f65f630a7b
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-04-28 16:13:05 +0800

    add users to this app

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+
+validates :username, presence: { message: "用户名不能为空" }
+validates :username, uniqueness: { message: "用户名已存在" }
+validates :password, presence: { message: "密码不能为空" }
+validates :password, length: { minimum: 6, message: "密码长度最短为6位" }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  root 'welcom#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+
+  resources :users
+  resources :sessions
+
+  namespace :admin do
+    root 'users#index'
+
+    resources :users do
+      collection do
+        get :search
+      end
+    end
+  end
+
 end
